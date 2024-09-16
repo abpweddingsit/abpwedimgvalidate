d526dae29dec0f262e496d4bcfca65082c4b0f2e
Abhigyan Sen <62685639+AbhigyanSen@users.noreply.github.com>
2024-09-16 17:58:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def process_image():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='0.0.0.0')
+    app.run(debug=False, port=5001, host='0.0.0.0')
