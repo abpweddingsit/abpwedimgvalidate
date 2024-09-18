e0a49232635740d295f821377abd2f5321b78410
Abhigyan Sen <62685639+AbhigyanSen@users.noreply.github.com>
2024-09-18 16:43:20 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from main2 import get_result
+
+app = Flask(__name__)
+@app.route('/process_image', methods=['POST'])
+
+def process_image():
+    try:
+        # Retrieve the JSON data from the request
+        data = request.get_json()
+        base64_image = data.get('base64_image')
+        
+        # Check if base64_image is provided
+        if not base64_image:
+            return jsonify({'error': 'No base64 image provided'}), 400
+        
+        # Call the get_result function to process the base64 image
+        final_result = get_result(base64_image)
+        
+        # Prepare the response
+        response = final_result
+        return jsonify(response), 200
+    except Exception as e:
+        # Handle unexpected errors
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=False, port=5001, host='0.0.0.0')
